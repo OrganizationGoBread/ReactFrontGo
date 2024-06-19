e8315b84cd4bb936925473cfc88a8d1e23d53114
Matheus Martins <110927871+MatheusMartins-1227@users.noreply.github.com>
2024-06-19 17:28:28 -0300

    Alterando e subindo a base URL com o dominio do site (HTTPS) incluso

--- goBreadFrontend/src/api/api.js
+++ goBreadFrontend/src/api/api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const ip = {
     local: "localhost:8080",
-    ec2: "44.215.34.206" 
+//  ec2: "44.215.34.206"
+    ec2: "gobread.zapto.org"
 }
 
 const api = axios.create({
-    baseURL: `http://${ip.ec2}/api`,
+    baseURL: `https://${ip.ec2}/api`,
     timeout: 8000,
 });
 
